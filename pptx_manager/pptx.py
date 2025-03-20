263204f3f41f88241f579bc842add0f7ca8ce12f
kirill <w_shkolenko@mail.ru>
2025-03-20 23:19:20 +0300

    Fixed imports

--- pptx_manager/pptx.py
+++ pptx_manager/pptx.py
@@ -1,9 +1,9 @@
-from pptx.font import FontManager
-from pptx.group import GroupManager
-from pptx.image import ImageManager
-from pptx.shape import ShapeManager
-from pptx.slide import SlideManager
-from pptx.table import TableManager
+from pptx_manager.font import FontManager
+from pptx_manager.group import GroupManager
+from pptx_manager.image import ImageManager
+from pptx_manager.shape import ShapeManager
+from pptx_manager.slide import SlideManager
+from pptx_manager.table import TableManager
 
 
 class PPTXManager(
