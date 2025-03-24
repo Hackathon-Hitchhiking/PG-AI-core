22518afff44940ea2d0d19f1d37d32b82bf2e43b
kirill <w_shkolenko@mail.ru>
2025-03-25 00:51:21 +0300

    Added the text frame parsing

--- pptx_manager/font.py
+++ pptx_manager/font.py
@@ -1,45 +1,51 @@
-from pptx import Presentation
-from pptx.slide import Slides
-
-from pptx_manager.models import FontElement, ParagraphFontElement, ShapeFontElement
-from pptx_manager.utils import get_all_methods, hex_to_rgb
-
-from pptx.dml.color import ColorFormat, RGBColor
-from pptx.enum.dml import MSO_THEME_COLOR
-from pptx.opc.constants import RELATIONSHIP_TYPE as RT
-from pptx.oxml import parse_xml
-from PIL import ImageColor
-import numpy as np
 import colorsys
 
+import numpy as np
+
 from loguru import logger
+from PIL import ImageColor
+from pptx import Presentation
+from pptx.opc.constants import RELATIONSHIP_TYPE as RT
+from pptx.oxml import parse_xml
+from pptx.shapes.autoshape import Shape
+from pptx.slide import Slides
+from pptx.text.text import Font
+
+from pptx_manager.models import ParagraphRunElement, RunElement, ShapeRunElement, TextFrameShape
+from pptx_manager.utils import get_all_methods, hex_to_rgb
+
 
 class FontManager:
     def __init__(self):
         self.pres = None
 
-    def get_all_fonts(self, slide: Slides) -> list[ShapeFontElement]:
+        self.text_frames = []
+
+    def get_all_runs(self, slide: Slides) -> list[ShapeRunElement]:
         shape_info_list = []
 
         for shape in slide.shapes:
+            print(type(shape))
             if not shape.has_text_frame:
                 continue
 
             paragraphs_list = []
             for paragraph in shape.text_frame.paragraphs:
                 font_list = []
+
+                logger.debug(f'methods = {get_all_methods(paragraph.text)}')
+
                 for run in paragraph.runs:
                     font = run.font
 
-                    logger.debug(f"font_name = {run.text}")
-                    logger.debug(f"methodds = {get_all_methods(run.part)}")
+                    font_size = 12
 
-                    font_size = font.size.pt
+                    if font.size is not None:
+                        font_size = font.size.pt
 
-                    font_color = self._get_font_color(slide, font.color)
+                    font_color = self._get_font_color(slide, font)
 
-                    run_info = FontElement(
-                        # TODO maybe here returning the language ID
+                    run_info = RunElement(
                         text=run.text,
                         font_name=font.name,
                         font_size=font_size,
@@ -52,25 +58,17 @@ class FontManager:
 
                 align_name = str(paragraph.alignment) if paragraph.alignment else None
 
-                paragraphs_list.append(ParagraphFontElement(
-                    alignment=align_name,
-                    fonts=font_list
-                ))
+                paragraphs_list.append(ParagraphRunElement(alignment=align_name, fonts=font_list))
 
-            shape_info = ShapeFontElement(
-                shape_id=shape.shape_id,
-                shape_name=shape.name,
-                paragraphs=paragraphs_list
-            )
+            shape_info = ShapeRunElement(shape_id=shape.shape_id, shape_name=shape.name, paragraphs=paragraphs_list)
             shape_info_list.append(shape_info)
 
         return shape_info_list
 
-    def _get_font_size(self):
-        # https://github.com/scanny/python-pptx/issues/378#issuecomment-2730305545
-
-    def _get_font_color(self, slide: Slides, font_color: ColorFormat,) -> tuple:
+    def _get_font_color(self, slide: Slides, font: Font) -> tuple:
         # https://stackoverflow.com/questions/54692768/python-pptx-read-font-color
+        font_color = font.color
+
         try:
             srgb = font_color.rgb
         except AttributeError:
@@ -91,15 +89,18 @@ class FontManager:
         brightness = font_color.brightness
 
         accent = theme_color.xml_value
-        xpath = 'a:themeElements/a:clrScheme/a:{}/a:srgbClr/@val'.format(accent)
+        xpath = f'a:themeElements/a:clrScheme/a:{accent}/a:srgbClr/@val'
 
         slide_master_part = slide.slide_layout.slide_master.part
         theme_part = slide_master_part.part_related_by(RT.THEME)
         theme = parse_xml(theme_part.blob)
 
-        hex_color = theme.xpath(xpath)[0]
+        try:
+            hex_color = theme.xpath(xpath)[0]
+        except IndexError:
+            return 0, 0, 0
 
-        srgb = np.array(ImageColor.getcolor('#{}'.format(hex_color), 'RGB'))
+        srgb = np.array(ImageColor.getcolor(f'#{hex_color}', 'RGB'))
 
         srgb = srgb / 255
         h, luminance, s = colorsys.rgb_to_hls(*srgb)
@@ -111,18 +112,72 @@ class FontManager:
 
         return tuple(srgb)
 
+    def _get_font_size(self, font: Font) -> float:
+        font_size = 12
+
+        if font.size is not None:
+            font_size = font.size.pt
+
+        return font_size
+
+    def parse_text_frame_shape(
+        self, slide_id: int, shape_id: int, slide: Slides, shape: Shape
+    ) -> None | TextFrameShape:
+        if shape.text == '':
+            return None
+        try:
+            text_frame = shape.text_frame
+
+            text = shape.text
+
+            paragraph = text_frame.paragraphs[0]
+            run = paragraph.runs[0]
+            font = run.font
+
+            font_size = self._get_font_size(font)
+            font_color = self._get_font_color(slide, font)
+            font_name = font.name
+            bold = font.bold
+            italic = font.italic
+            underline = font.underline
+        except Exception as e:
+            logger.warning(f'error parsing text_frame = {e}')
+            return None
+
+        text_frame = TextFrameShape(
+            slide_id=slide_id,
+            shape_id=shape_id,
+            text=text,
+            font_name=font_name,
+            font_size=font_size,
+            bold=bold,
+            italic=italic,
+            underline=underline,
+            color=font_color,
+            text_frame=text_frame,
+        )
+
+        self.text_frames.append(text_frame)
+
+        return text_frame
+
     def test(self, source):
         self.pres = Presentation(source)
 
+        shape_id = 1
+        slide_id = 1
         for slide in self.pres.slides:
-            logger.debug(self.get_all_fonts(slide))
+            for shape in slide.shapes:
+                if shape.has_text_frame:
+                    res = self.parse_text_frame_shape(slide_id, shape_id, slide, shape)
+                    if res is not None:
+                        shape_id += 1
+                        slide_id += 1
 
-
+        logger.debug(f'frames = {self.text_frames}')
 
 
 if __name__ == '__main__':
     fm = FontManager()
 
-    fm.test("../test_sources/test_dit.pptx")
-
-
+    fm.test('../test_sources/test_dit.pptx')
--- pptx_manager/models.py
+++ pptx_manager/models.py
@@ -1,7 +1,26 @@
+from typing import Any
+
 from pydantic import BaseModel
 
 
-class FontElement(BaseModel):
+class TextFrameShape(BaseModel):
+    slide_id: int
+    shape_id: int
+
+    text: str
+
+    font_name: str | None = None
+    font_size: float | None = None
+    bold: bool | None = None
+    italic: bool | None = None
+    underline: bool | None = None
+    strike: bool | None = None
+    color: tuple | None = None
+
+    text_frame: Any
+
+
+class RunElement(BaseModel):
     text: str
     font_name: str | None = None
     font_size: float | None = None
@@ -12,12 +31,12 @@ class FontElement(BaseModel):
     color: tuple | None = None
 
 
-class ParagraphFontElement(BaseModel):
+class ParagraphRunElement(BaseModel):
     alignment: str | None = None
-    fonts: list[FontElement]
+    fonts: list[RunElement]
 
 
-class ShapeFontElement(BaseModel):
+class ShapeRunElement(BaseModel):
     shape_id: int | None = None
     shape_name: str | None = None
-    paragraphs: list[ParagraphFontElement]
+    paragraphs: list[ParagraphRunElement]
