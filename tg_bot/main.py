472a1b886db1035afc3b8cff9cd026cffe498264
kirill <w_shkolenko@mail.ru>
2025-04-01 20:01:14 +0300

    Added the tg bot

---
+++ tg_bot/main.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from app.external_api import ExternalAPI
+from app.handlers import router
+from config import API_TOKEN
+
+
+storage = MemoryStorage()
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(storage=storage)
+external_api = ExternalAPI(bot, storage)
+
+
+async def main():
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот завершил работу')
